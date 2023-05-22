13e413f749afe09e47ab15d83c4bb2480c3a6dd2
Mike <ricoengelmiguel@gmail.com>
2023-05-22 22:34:07 +0200

    laishjdf

--- db/index.js
+++ db/index.js
@@ -6,7 +6,31 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/basic-auth";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mikey6:<Nomadasperdidos#3>@mikey6.l8g62xy.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 mongoose
   .connect(MONGO_URI)
